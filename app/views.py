b35b7b723227489a890ce052f271a65d9d326af5
Kimberly Laughton <kimberlyclaughton@gmail.com>
2023-02-19 21:19:57 -0500

    Completed styling

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from app import mail
+from flask_mail import Message
+from .forms import ContactForm
 
 ###
 # Routing for your application.
@@ -16,6 +18,27 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            subject = form.subject.data
+            message = form.message.data
+
+            msg = Message(subject, sender=(name, email), recipients=["kimberlyclaughton@gmail.com"])
+            msg.body = message
+            mail.send(msg)
+            flash('Email successfully delivered', 'success')
+            return redirect(url_for('home'))
+        
+        flash_errors(form)
+
+    return render_template('contact.html', form=form)
 
 
 ###
